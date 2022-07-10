06bbc2c1937089e7c6bd1884345246dbb6ffe636
Lex Lim <hyperzlink@outlook.com>
2022-07-10 23:05:45 +0800

    增加用户参数配置

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -1,26 +1,43 @@
 <?php
 namespace Isekai\OffcanvasToc;
 
+use MediaWiki\MediaWikiServices;
 use Html;
 
 class Hooks {
     public static function onLoad(\OutputPage $outputPage) {
-        $outputPage->enableOOUI();
-        $outputPage->addModules('ext.isekai.offcanvas-toc');
-        $outputPage->addModules('oojs-ui.styles.icons-layout');
+		$service = MediaWikiServices::getInstance();
+		if (
+            $outputPage->getTitle()->isContentPage() &&
+            $service->getUserOptionsLookup()->getOption($outputPage->getUser(), 'offcanvas-toc-enabled')
+        ) {
+            $outputPage->enableOOUI();
+            $outputPage->addModules('ext.isekai.offcanvas-toc');
+            $outputPage->addModules('oojs-ui.styles.icons-layout');
 
-        $outputPage->addHTML(Html::openElement('div', [
-            'id' => 'isekai-offcanvas-toc',
-            'class' => 'toc-offcanvas'
-        ]) . Html::element('ul') . Html::closeElement('div'));
+            $outputPage->addHTML(Html::openElement('div', [
+                'id' => 'isekai-offcanvas-toc',
+                'class' => 'toc-offcanvas'
+            ]) . Html::element('ul') . Html::closeElement('div'));
 
-        $outputPage->addHTML(Html::openElement('button', [
-            'id' => 'iseai-offcanvas-btn',
-            'class' => 'toc-offcanvas-btn'
-        ]) . new \OOUI\IconWidget([
-            'icon' => 'menu'
-        ]) . Html::closeElement('button'));
+            $outputPage->addHTML(Html::openElement('button', [
+                'id' => 'iseai-offcanvas-btn',
+                'class' => 'toc-offcanvas-btn'
+            ]) . new \OOUI\IconWidget([
+                'icon' => 'menu'
+            ]) . Html::closeElement('button'));
 
-        $outputPage->addElement('div', ['id' => 'isekai-offcanvas-cover', 'class' => 'toc-offcanvas-cover']);
+            $outputPage->addElement('div', ['id' => 'isekai-offcanvas-cover', 'class' => 'toc-offcanvas-cover']);
+        }
+    }
+
+    public static function onGetPreferences(\User $user, array &$preferences) {
+        $preferences['offcanvas-toc-enabled'] = [
+			'type' => 'toggle',
+			'label-message' => 'offcanvas-toc-enabled',
+			'section' => 'rendering/offcanvas-toc'
+		];
+
+        return true;
     }
 }
